c141d074b1bbade23c662de3ae8c7f803e2d2e7a
slim1979 <chinezan@yandex.ru>
2017-05-10 16:09:06 +0300

    настроены тесты для содержимого и заголовков страниц Home, Help, About us. Добавлена страница About Us

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  @basic_title = 'Ruby on Rails Tutorial Sample App'
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+
+    it 'должен быть заголовок Ruby on Rails Tutorial Sample App | Home' do
+      visit '/static_pages/home'
+      expect(page).to have_title(" #{@basic_title} | Home")
     end
   end
   describe "Help page" do
@@ -15,5 +22,25 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Vishenka')
     end
+
+    it 'должен быть заголовок Ruby on Rails Tutorial Sample App | Help' do
+      visit '/static_pages/help'
+      expect(page).to have_title(" #{@basic_title} | Help")
+    end
+
   end
+
+  describe "About page" do
+
+  it "should have the content 'About Us'" do
+    visit '/static_pages/about'
+    expect(page).to have_content('About Us')
+  end
+
+  it 'должен быть заголовок Ruby on Rails Tutorial Sample App | About' do
+    visit '/static_pages/about'
+    expect(page).to have_title(" #{@basic_title} | About")
+  end
+end
+
 end
