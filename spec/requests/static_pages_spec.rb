d0f2110d109ec087ff92f739f5b0b8ed77cbea6a
slim1979 <chinezan@yandex.ru>
2017-05-26 23:09:22 +0300

    rspec --format doc

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,35 +1,32 @@
 require 'spec_helper'
 require 'rails_helper'
 
-describe "Static pages" do
-
+describe 'Static pages' do
   @basic_title = 'Ruby on Rails Tutorial Sample App'
 
   subject { page }
 
-  describe "Home page" do
+  describe 'Home page' do
     before { visit root_path }
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | Home") }
+    it { should have_title('Ruby on Rails Tutorial Sample App | Home') }
   end
 
-  describe "Help page" do
-    before {visit help_path}
-    it {should have_content('Vishenka')}
-    it {should have_title("Ruby on Rails Tutorial Sample App | Help")}
+  describe 'Help page' do
+    before { visit help_path }
+    it { should have_content('Vishenka') }
+    it { should have_title('Ruby on Rails Tutorial Sample App | Help') }
   end
 
-
-  describe "About page" do
-    before {visit about_path}
-    it {should have_content('About us')}
-    it {should have_title("Ruby on Rails Tutorial Sample App | About")}
+  describe 'About page' do
+    before { visit about_path }
+    it { should have_content('About us') }
+    it { should have_title('Ruby on Rails Tutorial Sample App | About') }
   end
 
-  describe "Contacts page" do
-    before {visit contacts_path}
-    it {should have_content('Contacts')}
-    it {should have_title("Ruby on Rails Tutorial Sample App | Contacts")}
+  describe 'Contacts page' do
+    before { visit contacts_path }
+    it { should have_content('Contacts') }
+    it { should have_title('Ruby on Rails Tutorial Sample App | Contacts') }
   end
-
 end
